40eeff18eb9d3b25ce034266b1b09fe5a0293f30
angelopoole <angelo.poole@outlook.com>
2020-11-21 18:46:38 -0500

    added asssesment

--- reactGuideS1/src/App.js
+++ reactGuideS1/src/App.js
@@ -1,24 +1,46 @@
 import './App.css';
 import Person from './Person/Person';
 import React, { useState, useEffect } from 'react';
+import person from './Person/Person';
 
 function App() {
 	let [counter, setCounter] = useState(0);
 
+	const [showPerson, setShowPerson] = useState(false);
+
 	let [people, setPeople] = useState({
 		persons: [
-			{ name: 'angelo', age: 21 },
-			{ name: 'victoria', age: 20 },
-			{ name: 'booboo', age: 200 },
+			{ id: Math.floor(Math.random() * 100) + 1, name: 'angelo', age: 21 },
+			{ id: Math.floor(Math.random() * 100) + 1, name: 'victoria', age: 20 },
+			{ id: Math.floor(Math.random() * 100) + 1, name: 'booboo', age: 200 },
 		],
 	});
 
+	// console.log(people);
+
 	const listAllPeople = people => {
 		// You have to return the map function at the top level aswell. not just the output of the map function.
-		return people.persons.map(person => (
-			<Person name={person.name} age={person.age} />
+		return people.persons.map((person, index) => (
+			<Person
+				key={person.id}
+				id={person.id}
+				name={person.name}
+				age={person.age}
+				// logObject={loggerHandler}
+			/>
 		));
 	};
+
+	// const loggerHandler = e => {
+	// 	let { name, age, id } = e;
+	// 	let found = people.persons.filter(el => el.id == id);
+
+	// 	setPeople(el => console.log(el));
+	// 	// let removeAge = age => {
+	// 	// 	return setPeople(...people, (age = 0));
+	// 	// };
+	// 	// return removeAge;
+	// };
 
 	const counterCountUp = () => {
 		counter = setInterval(() => {
@@ -30,13 +52,16 @@ function App() {
 		counterCountUp();
 	}, []);
 
-	// console.log(people.persons.forEach(a => console.log(a.name, a.age)));
+	let togglePersonsHandler = e => {
+		setShowPerson(val => (val = !val));
+	};
 
 	return (
 		<div className='App'>
 			<h1>Hi im a React app</h1>
-			<h1>page timer -> {counter}</h1>
-			<div> -> {listAllPeople(people)}</div>
+			<h1>page timer: {counter}</h1>
+			<button onClick={togglePersonsHandler}> toggle people</button>
+			{showPerson ? <div>{listAllPeople(people)}</div> : null}
 		</div>
 	);
 }
--- reactGuideS1/src/Person/Person.js
+++ reactGuideS1/src/Person/Person.js
@@ -2,12 +2,14 @@
 // you no longer have to import react to use jsx features in a react project. you only need to import it if you're using something that extends the React object (React.w/e)
 
 const person = props => {
-	let { name, age } = props;
-
+	let { name, age, id, logObject } = props;
+	// console.log(id);
 	return (
+		// <div onClick={e => logObject(props)}>
 		<h1>
-			Hi! im a {name} and my age is {age}!
+			Hi! im a {name} and my age is {age}! my index is {id}
 		</h1>
+		// </div>
 	);
 };
 
