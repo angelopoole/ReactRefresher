cea95a445bfc288f19b0e0f292793377e2ab251c
angelopoole <angelo.poole@outlook.com>
2020-11-21 11:05:47 -0500

    added notes on importing functions and jsx into person/person.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>Hi im a React app</h1>
+			<Person />
 		</div>
 	);
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,8 @@
+// import React from 'react';
+// you no longer have to import react to use jsx features in a react project. you only need to import it if you're using something that extends the React object (React.w/e)
+
+const person = () => {
+	return <h1> Hi! im a person! </h1>;
+};
+
+export default person;
