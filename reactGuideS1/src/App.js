48c6dafa8575fc2f79cb9a57571c94fc5d2fef3c
angelopoole <angelo.poole@outlook.com>
2020-11-21 14:01:45 -0500

    Changed file structure

--- reactGuideS1/src/App.js
+++ reactGuideS1/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Person from './Person/Person';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-	const [people, setPeople] = useState({
+	let [counter, setCounter] = useState(0);
+
+	let [people, setPeople] = useState({
 		persons: [
 			{ name: 'angelo', age: 21 },
 			{ name: 'victoria', age: 20 },
@@ -13,17 +15,27 @@ function App() {
 
 	const listAllPeople = people => {
 		// You have to return the map function at the top level aswell. not just the output of the map function.
-
 		return people.persons.map(person => (
 			<Person name={person.name} age={person.age} />
 		));
 	};
+
+	const counterCountUp = () => {
+		counter = setInterval(() => {
+			setCounter(counter => counter + 1);
+		}, 1000);
+	};
+
+	useEffect(() => {
+		counterCountUp();
+	}, []);
 
 	// console.log(people.persons.forEach(a => console.log(a.name, a.age)));
 
 	return (
 		<div className='App'>
 			<h1>Hi im a React app</h1>
+			<h1>page timer -> {counter}</h1>
 			<p> -> {listAllPeople(people)}</p>
 		</div>
 	);
