d08de654be4dee2b9fda9ff1d94f31237cd4a738
angelopoole <angelo.poole@outlook.com>
2020-11-21 12:00:34 -0500

    refresher on building and generating dynamic props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import Person from './Person/Person';
+import React, { useState } from 'react';
 
 function App() {
+	const [people, setPeople] = useState({
+		persons: [
+			{ name: 'angelo', age: 21 },
+			{ name: 'victoria', age: 20 },
+			{ name: 'booboo', age: 200 },
+		],
+	});
+
+	const listAllPeople = people => {
+		// You have to return the map function at the top level aswell. not just the output of the map function.
+
+		return people.persons.map(person => (
+			<Person name={person.name} age={person.age} />
+		));
+	};
+
+	// console.log(people.persons.forEach(a => console.log(a.name, a.age)));
+
 	return (
 		<div className='App'>
 			<h1>Hi im a React app</h1>
-			<Person />
+			<p> -> {listAllPeople(people)}</p>
 		</div>
 	);
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,14 @@
 // import React from 'react';
 // you no longer have to import react to use jsx features in a react project. you only need to import it if you're using something that extends the React object (React.w/e)
 
-const person = () => {
-	return <h1> Hi! im a person! </h1>;
+const person = props => {
+	let { name, age } = props;
+
+	return (
+		<h1>
+			Hi! im a {name} and my age is {age}!
+		</h1>
+	);
 };
 
 export default person;
